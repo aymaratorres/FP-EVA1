6011f90714f080d1ce74bbab28e57b130e84c799
aymaratorres <L25550860@chihuahua2.tecnm.mx>
2025-08-27 22:30:29 -0600

    Add files via upload

---
+++ EVA1_PRACTICA2_DATOS/src/eva1_practica2_datos/EVA1_PRACTICA2_DATOS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica2_datos;
+
+/**
+ *
+ * @author Aymara
+ */
+public class EVA1_PRACTICA2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("AYMARA TORRES");
+        // T"-->CADENA DE TEXTO"
+        System.out.println(1000);
+        //NUMEROS ENTEROS
+        System.out.println(500.3);
+        //NUMEROS DECIMALES
+        System.out.println(true);
+                //VALORES LOGICOS (VERDADERO O FALSO)
+                //true false
+                System.out.println('c');//CARACTER
+                        System.out.println("c");//CADENA DE TEXTO
+                        //CARACTERES INDIVIDUALES
+                        //COSAS DISTINTAS
+                       
+                        
+                    
+    }
+    
+}
