766fbf6868f7d7ca07c6342548741b825cd2b78b
aymaratorres <L25550860@chihuahua2.tecnm.mx>
2025-09-02 04:13:20 -0600

    Add files via upload

---
+++ EVA1_7_AREA_TRIANGULO/src/eva1_7_area_triangulo/EVA1_7_AREA_TRIANGULO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_area_triangulo;
+
+/**
+ *
+ * @author Aymara
+ */
+public class EVA1_7_AREA_TRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double base, altura, area; //todas son doubles
+       base = 8;
+       altura = 6;
+       area = (base * altura) / 2;
+       System.out.println("Area de un triangulo base = 8 y altura = 6:");
+       System.out.println(area);
+       
+        
+    }
+    
+}
