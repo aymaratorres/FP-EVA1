fcbdb11f2a6e757108629056819897755f05df20
aymaratorres <L25550860@chihuahua2.tecnm.mx>
2025-09-02 04:03:01 -0600

    Add files via upload

---
+++ EVA1_4_EJERCICIO/src/eva1_4_ejercicio/EVA1_4_EJERCICIO.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_ejercicio;
+
+/**
+ *
+ * @author Aymara
+ */
+public class EVA1_4_EJERCICIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.print("Empleo: ");
+    System.out.println("Personal de limpieza en escuela");
+    System.out.print("Salario: ");
+    System.out.println("4000 mensuales");
+    System.out.print("Horas semana: ");
+    System.out.println("8 horas");
+    System.out.print("Requiere inglés: ");
+    System.out.println("false");
+    
+    
+    
+    }
+    
+}
