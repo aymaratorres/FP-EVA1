4dc21b2388331ca3ec03f1df8d4e67923ceec7a9
aymaratorres <L25550860@chihuahua2.tecnm.mx>
2025-09-20 04:58:03 -0600

    Add files via upload

---
+++ EVA1_15_MAYOR/src/eva1_15_mayor/EVA1_15_MAYOR.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_mayor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aymara
+ */
+public class EVA1_15_MAYOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int num1, num2;
+    Scanner input = new Scanner (System.in);
+    System.out.println("Numero 1:");
+    num1 = input.nextInt();
+    System.out.println("Numero 2:");
+    num2 = input.nextInt();
+    if(num1 > num2){
+        System.out.println("El valor mayor es:");
+        System.out.println(num1);
+    }else{
+    if(num2 > num1){
+        System.out.println("El valor mayor es:");
+        System.out.println(num2);
+    }else{
+        System.out.println("Los valores son iguales;");
+    }
+    }
+}
+}
