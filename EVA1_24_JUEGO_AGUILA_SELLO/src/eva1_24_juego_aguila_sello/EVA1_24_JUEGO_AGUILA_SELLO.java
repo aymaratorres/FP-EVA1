2048324e6a3e5ef8a72b62ac67e0f6ffb40c30b4
aymaratorres <L25550860@chihuahua2.tecnm.mx>
2025-09-23 16:22:22 -0600

    Add files via upload

---
+++ EVA1_24_JUEGO_AGUILA_SELLO/src/eva1_24_juego_aguila_sello/EVA1_24_JUEGO_AGUILA_SELLO.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_24_juego_aguila_sello;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aymara
+ */
+public class EVA1_24_JUEGO_AGUILA_SELLO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner captu = new Scanner (System.in);
+    int Usuario;
+    //System.out.println("Aguila o sello? (1/0):");
+    //Usuario = captu.nextInt();
+    System.out.println(Math.random());
+    //Imprimir que cae la moneda (Aguila o Sello) segun
+    //El valor de random
+    Usuario = captu.nextInt();
+    
+    
+   
+    }
+    
+    
+}
